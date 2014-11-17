57e519365ce682462ef2b215d81acd1a95d804af
Gabriel Ferreira <gabriel.d3v@gmail.com>
2014-11-16 22:25:20 -0200

    modificando layout

--- araujotransporte/contact.php
+++ araujotransporte/contact.php
@@ -1,6 +1,6 @@
 <?php
 
-//Retrieve form data. 
+//Retrieve form data.
 //GET - user submitted data using AJAX
 //POST - in case user does not support javascript, we'll use POST instead
 $name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
@@ -12,37 +12,33 @@ $comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
 if ($_POST) $post=1;
 
 //Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
+if (!$name) $errors[count($errors)] = 'Entre com seu nome.';
+if (!$email) $errors[count($errors)] = 'Entre com seu email.';
+if (!$comment) $errors[count($errors)] = 'Entre com sua mensagem.';
 
 //if the errors array is empty, send the mail
 if (!$errors) {
 
-	//recipient - replace your email here
-	$to = 'wowthemesnet@gmail.com';	
-	//sender - from the form
+	$to = 'contato@araujotransporte.com';
 	$from = $name . ' <' . $email . '>';
-	
-	//subject and the html message
-	$subject = 'Message from ' . $name;	
-	$message = 'Name: ' . $name . '<br/><br/>
-		       Email: ' . $email . '<br/><br/>		
-		       Message: ' . nl2br($comment) . '<br/>';
+	$subject = 'Mensagem de ' . $name;
+	$message = 'Nome: ' . $name . '<br/><br/>
+		       Email: ' . $email . '<br/><br/>
+		       Mensagem: ' . nl2br($comment) . '<br/>';
 
 	//send the mail
 	$result = sendmail($to, $subject, $message, $from);
-	
+
 	//if POST was used, display the message straight away
 	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
+		if ($result) echo 'Obrigado! Recebemos sua mensagem.';
+		else echo 'Desculpe, ocorreu um erro. Por favor, tente mais tarde.';
+
+	//else if GET was used, return the boolean value so that
 	//ajax script can react accordingly
 	//1 means success, 0 means failed
 	} else {
-		echo $result;	
+		echo $result;
 	}
 
 //if the errors array has values
@@ -59,9 +55,9 @@ function sendmail($to, $subject, $message, $from) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 	$headers .= 'From: ' . $from . "\r\n";
-	
+
 	$result = mail($to,$subject,$message,$headers);
-	
+
 	if ($result) return 1;
 	else return 0;
 }
